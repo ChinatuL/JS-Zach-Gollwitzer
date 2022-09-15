7f242c62ff9b0e3d927c0c961851510e5f80cecb
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-09-15 22:17:27 +0100

    add solution to challenge 13

---
+++ rock_paper_scissors.js
@@ -0,0 +1,19 @@
+// Challenge 13
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+// rock beats scissors, scissors beats paper, paper beats rock.
+
+// Solution
+const rps = (p1, p2) => {
+    if (p1 == p2) {
+        return "Draw!";
+    }
+    if (p1 == "rock" && p2 == "scissors") {
+        return "Player 1 won!";
+    } else if (p1 == "scissors" && p2 == "paper") {
+        return "Player 1 won!";
+    } else if (p1 == "paper" && p2 == "rock") {
+        return "Player 1 won!";
+    } else {
+        return "Player 2 won!";
+    }
+};
