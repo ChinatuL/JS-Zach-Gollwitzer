dc233dbbd6d66f0e5c4ebe4f23ab3a8945d0c834
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-09-15 21:55:58 +0100

    add solution to challenge 11

---
+++ grasshopper_summation.js
@@ -0,0 +1,13 @@
+// Challenge 11;
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// Solution
+
+const summation = function (num) {
+    // Code here
+    let result = 0;
+    for (let i = 1; i <= num; i++) {
+        result += i;
+    }
+    return result;
+};
