a5b03fbd84351306dd0861c972b3973d9b35fb80
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-09-16 11:24:38 +0100

    add solution to challenge 18

---
+++ Beginner/convert_a_string_to_an_array.js
@@ -0,0 +1,9 @@
+// Challenge 18
+
+// Write a function to split a string and convert it into an array of words.
+
+// Solution
+function stringToArray(string) {
+    return string.split(" ");
+}
+console.log(stringToArray("I love arrays they are my favorite"));
