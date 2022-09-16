e9eeee3feb38aee79ab01122fb511ecb8f82cad7
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-09-16 09:22:24 +0100

    add solution to challenge 17

---
+++ Beginner/string_repeat.js
@@ -0,0 +1,12 @@
+// Challenge 17
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Solution
+function repeatStr(n, s) {
+    let repeatedString = "";
+    for (let i = 0; i < n; i++) {
+        repeatedString += s;
+    }
+    return repeatedString;
+}
+console.log(repeatStr(5, "I"));
