395dce67d8cdfda9545c674197e4323c1f567b6a
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-09-16 12:13:11 +0100

    add solution to challenge 23

---
+++ Beginner/century_from_year.js
@@ -0,0 +1,5 @@
+// Challenge 23
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Solution
+const century = (year) => Math.ceil(year / 100);
